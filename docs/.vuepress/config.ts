e72727f46516475dba1e5f2ce75231ca4abf81ef
AustinDeng <nidhogg.dxx@gmail.com>
2023-06-18 01:42:20 +0800

    test github pages

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     '/': {
       lang: 'zh-CN',
       title: "My Notebook",
-      description: 'Just for fun',
+      description: 'Just for playing',
     }
   },
   base: '/Notebook/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
